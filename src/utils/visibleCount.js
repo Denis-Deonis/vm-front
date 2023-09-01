b4628cbc499b7ca30e1fb9eea8e090ab8a6dd7a5
Denis Zykov <denis.zykov.1993@gmail.com>
2023-09-01 14:38:29 +0300

    width 1200

--- src/utils/visibleCount.js
+++ src/utils/visibleCount.js
@@ -5,9 +5,9 @@ export const getVisibleCount = () => {
     return {loadCards: 5, moreCards: 2}
   } else if (width >= 653 && width < 767) {
     return {loadCards: 8, moreCards: 2}
-  } else if (width > 767) {
+  } else if (width >= 767 && width < 1200) {
     return {loadCards: 8, moreCards: 2}
-  }else if (width > 1023) {
+  } else if (width >= 1200) {
     return {loadCards: 12, moreCards: 3}
   }
 }
